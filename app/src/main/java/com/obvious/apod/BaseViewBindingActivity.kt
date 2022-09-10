6ff1b76d08530c5afda807c03000fb8377701277
ljk15 <jk.lijin@gmail.com>
2022-09-10 19:44:04 +0530

    Add base class for viewbinding
    Add useful dependencies

---
+++ app/src/main/java/com/obvious/apod/BaseViewBindingActivity.kt
@@ -0,0 +1,21 @@
+package com.obvious.apod
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+typealias BindingFactory<T> = (LayoutInflater) -> T
+
+abstract class BaseViewBindingActivity<VB : ViewBinding>(
+    private val bindingFactory: BindingFactory<VB>,
+) : AppCompatActivity() {
+
+    protected lateinit var binding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/obvious/apod/LandingActivity.kt
@@ -0,0 +1,13 @@
+package com.obvious.apod
+
+import android.os.Bundle
+import com.obvious.apod.databinding.ActivityLandingBinding
+
+class LandingActivity :
+    BaseViewBindingActivity<ActivityLandingBinding>(ActivityLandingBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
